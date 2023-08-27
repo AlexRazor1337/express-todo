1872238021ea59050aeab2853dc771d14a522596
AlexRazor <alexrazor1337@gmail.com>
2023-08-27 23:35:29 +0300

    Finish TODO app and google login

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ mkdirp.sync(DB_PATH);
 const db = new sqlite3.Database(`${DB_PATH}/db.sqlite`);
 
 db.serialize(() => {
-   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password BLOB, salt BLOB, email TEXT UNIQUE, email_verified INTEGER)'); 
+   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password BLOB, salt BLOB, name TEXT, email TEXT UNIQUE, email_verified INTEGER)'); 
    db.run('CREATE TABLE IF NOT EXISTS federated_credentials (id INTEGER PRIMARY KEY, user_id INTEGER, provider TEXT NOT NULL, subject TEXT NOT NULL, UNIQUE (provider, subject))');
    db.run('CREATE TABLE IF NOT EXISTS exercises (id INTEGER PRIMARY KEY, owner_id INTEGER NOT NULL, title TEXT NOT NULL, completed INTEGER)');
    
--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,126 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as GoogleStrategy } from 'passport-google-oidc';
 import passport from 'passport';
 import crypto from 'crypto';
 import db from '../db.js';
 
 const router = express.Router();
 
-
-passport.use(new LocalStrategy(function verify(username, password, cb){
-    db.get('SELECT * FROM users WHERE username = ?', [username], function(err, row) {
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+    db.get('SELECT * FROM users WHERE username = ?', [username], function (err, row) {
         if (err) { return cb(err); }
-        if (!row) { return cb(null, false, { message: 'Incorrect username or password.'});}
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
 
-    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha512', function(err, hashedPassword) {
-        if (err)  { return cb(err); }
-        if (!crypto.timingSafeEqual(row.password, hashedPassword)) {
-            return cb(null, false, {message: 'Incorrect username or password.'});
-        }
-        return cb(null, row);
-    });
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha512', function (err, hashedPassword) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(row.password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+            }
+            return cb(null, row);
+        });
     });
 }));
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.G_CLIENT_ID,
+    clientSecret: process.env.G_SECRET,
+    callbackURL: '/oauth2/redirect/google',
+    scope: [ 'profile' ]
+  }, function verify(issuer, profile, cb) {
+    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+      issuer,
+      profile.id
+    ], function(err, row) {
+      if (err) { return cb(err); }
+      if (!row) {
+        db.run('INSERT INTO users (name) VALUES (?)', [
+          profile.displayName
+        ], function(err) {
+          if (err) { return cb(err); }
+          var id = this.lastID;
+          db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+            id,
+            issuer,
+            profile.id
+          ], function(err) {
+            if (err) { return cb(err); }
+            var user = {
+              id: id,
+              name: profile.displayName
+            };
+            return cb(null, user);
+          });
+        });
+      } else {
+        db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+          if (err) { return cb(err); }
+          if (!row) { return cb(null, false); }
+          return cb(null, row);
+        });
+      }
+    });
+  }));
+    
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, { id: user.id, username: user.username });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
 
 router.get('/login', (req, res, next) => {
     res.render('login');
 });
 
+router.get('/login/federated/google', passport.authenticate('google', { scope: ['openid', 'email', 'profile'] }));
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+}));
 
 router.post('/login/password', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
 }));
+
+router.post('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) return next(err);
+        res.redirect('/');
+    });
+});
+
+router.get('/signup', function(req,res,next) {
+    res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha512', function (err, hashedPassword) {
+        if (err) return next(err);
+        db.run('INSERT INTO users (username, password, salt) VALUES (?, ?, ?)', [req.body.username, hashedPassword, salt], function (err) {
+            if (err) return next(err);
+            const user = {
+                id: this.lastID,
+                username: req.body.username
+            };
+            req.login(user, function (err) {
+                if (err) return next(err);
+                return res.redirect('/');
+            });
+        });
+    });
+});
+
 export default router;
